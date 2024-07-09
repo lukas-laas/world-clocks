a22b3561794e614da6eef7a2139828f198888815
lukas-laas <lukas.s.laas@gmail.com>
2024-07-09 14:26:35 +0200

    uppercase custom type

--- src/components/clock/clock.tsx
+++ src/components/clock/clock.tsx
@@ -1,12 +1,12 @@
 import { toZonedTime, format } from "date-fns-tz";
 import "./clock.css";
 
-type props = {
+type Props = {
   timeZone: string;
   time: Date;
 };
 
-export const Clock = ({ timeZone, time }: props) => {
+export const Clock = ({ timeZone, time }: Props) => {
   const zonedTime = toZonedTime(time, timeZone);
   const pattern = "HH:mm:ss";
   const city = timeZone.replace("_", " ").split("/")[1];
