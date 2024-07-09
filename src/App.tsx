2089ce737b049a61f98474e3f23a23062a149235
lukas-laas <lukas.s.laas@gmail.com>
2024-07-09 11:25:34 +0200

    move board to seperate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import { Clock } from "./components/clock/clock";
+import { Board } from "./components/board/board";
 
 function App() {
-  const timeZones: string[] = ["Europe/Stockholm", "Europe/Helsinki"];
-  const [date, setDate] = useState(new Date().getTime());
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setDate(new Date().getTime());
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
   return (
     <>
       <h1>World Clocks</h1>
-      {timeZones.map((timeZone, i) => {
-        return <Clock key={i} timeZone={timeZone} time={date}></Clock>;
-      })}
+      <Board></Board>
     </>
   );
 }
---
+++ src/components/board/board.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { Clock } from "../clock/clock";
+
+export function Board() {
+  const timeZones: string[] = ["Europe/Stockholm", "Europe/Helsinki"];
+  const [date, setDate] = useState(new Date().getTime());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date().getTime());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <>
+      {timeZones.map((timeZone, i) => {
+        return <Clock key={i} timeZone={timeZone} time={date}></Clock>;
+      })}
+    </>
+  );
+}
