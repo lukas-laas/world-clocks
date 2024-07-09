ee783975bae73f828ab549667d36c9200c713aaf
lukas-laas <lukas.s.laas@gmail.com>
2024-07-09 14:59:03 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/world-clocks",
+  build: {
+    target: "esnext", //browsers can handle the latest ES features
+  },
+});
